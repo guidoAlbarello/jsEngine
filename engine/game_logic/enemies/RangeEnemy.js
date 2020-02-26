abfe17f22075fd3aa9aa6aa480bb78cfc6dcc41d
LeoCenturion <centurionm.leo@gmail.com>
2020-02-25 22:45:18 -0300

    fix in distance calculation

--- engine/game_logic/enemies/RangeEnemy.js
+++ engine/game_logic/enemies/RangeEnemy.js
@@ -1,8 +1,8 @@
 function distance(p1, p2){
     return Math.sqrt(
 	(p1[0]-p2[0])**2
-	    +(p1[1] + p2[1])**2
-	    + (p1[2] + p2[2])**2);
+	    +(p1[1] - p2[1])**2
+	    + (p1[2] - p2[2])**2);
 }
 
 function dotProduct(p1, p2){
