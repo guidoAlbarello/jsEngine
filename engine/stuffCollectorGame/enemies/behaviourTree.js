e9449ee0867f16b879703ef517de26f5531560ab
LeoCenturion <centurionm.leo@gmail.com>
2020-03-23 19:57:43 -0300

    Basic behaviour tree added.

---
+++ engine/stuffCollectorGame/enemies/behaviourTree.js
@@ -0,0 +1,82 @@
+returnStatement = {
+  SUCCESS: 'success',
+  FAILED: 'fail',
+  RUNNING: 'running',
+};
+
+class DecoratorNode {
+  constructor(condition, child) {
+    this.condition = condition;
+    this.child = child;
+  }
+
+  tick(blackboard) {
+    if (this.condition()) {
+      return this.child.tick(blackboard);
+    }
+
+    return returnStatement.SUCCESS;
+  }
+}
+
+class CompositeNode {
+
+  //decorators is a Map instance wit ('tag', decorator instance) pairs
+  constructor(decorators) {
+    this.decorators = decorators;
+  }
+
+  tick(blackboard) {
+    //excecutes all decorators until one returns as running
+    for (var tag of this.decorators.keys()) {
+
+      let ret = this.decorators.get(tag).tick(blackboard);
+      if (ret == returnStatement.RUNNING) {
+        return ret;
+      }
+    }
+
+    return returnStatement.SUCCESS;
+  }
+
+  //d = ('tag', instance)
+  addDecorator(d) {
+    this.decorators[d[0]] = d[1];
+  }
+
+  removeDecorator(tag) {
+    this.decorators.delete(tag);
+  }
+
+}
+
+class ActionNode {
+  constructor(action) {
+    this.action = action;
+  }
+
+  tick(blackboard) {
+    let ret = this.action();
+    if (ret === returnStatement.RUNNING) {
+      blackboard.running = this;
+    }
+
+    return ret;
+  }
+}
+
+class BehaviourTree {
+  constructor(root) {
+    this.root = root;
+    this.running = root;
+  }
+
+  tick() {
+    let ret = this.running.tick(blackboard);
+    if (this.running === null) {
+      this.running = this.root;
+    }
+
+  }
+
+}
---
+++ engine/tests/behaviourTreeTest.js
@@ -0,0 +1,101 @@
+function assertEQ(x, y, errmsg) {
+  if (x != y) {
+    console.log('%cTEST FAILED ' + errmsg, 'color: red');
+    console.log(x + ' not equal to ' + y);
+    console.log('\n');
+  } else {
+    console.log('%cTEST PASSED ' + errmsg, 'color: green');
+
+  }
+}
+
+function testActionNodes() {
+  let failsInOneTick = new ActionNode(() => {
+    return returnStatement.FAILED;
+  });
+
+  let successInOneTick = new ActionNode(() => {
+    return returnStatement.SUCCESS;
+  });
+
+  n = 4; // en este caso n es infomación externa al arbol
+  let successAfterN = new ActionNode(() => {
+    //console.log('n = ' + n);
+    if (n == 0) {
+      return returnStatement.SUCCESS;
+    }
+
+    n = n - 1;
+    return returnStatement.RUNNING;
+  });
+
+  let blackboard = {};
+
+  assertEQ(returnStatement.SUCCESS,
+    successInOneTick.tick(blackboard),
+    'Success in one tick');
+
+  assertEQ(returnStatement.FAILED,
+    failsInOneTick.tick(blackboard),
+    'Fail in one tick');
+
+  assertEQ(returnStatement.RUNNING, successAfterN.tick(blackboard), 'Success in five ticks n=4');
+  assertEQ(returnStatement.RUNNING, successAfterN.tick(blackboard), 'Success in five ticks n=3');
+  assertEQ(returnStatement.RUNNING, successAfterN.tick(blackboard), 'Success in five ticks n=2');
+  assertEQ(returnStatement.RUNNING, successAfterN.tick(blackboard), 'Success in five ticks n=1');
+  assertEQ(returnStatement.SUCCESS, successAfterN.tick(blackboard), 'Success in five ticks n=0');
+}
+
+function testDecoratorNodes() {
+  console.log('\n DECORATOR TESTS');
+  let action = new ActionNode(() => {
+    return returnStatement.RUNNING;
+  });
+  let child = new CompositeNode(new Map(
+    [
+      ['action', action],
+    ])); //so it fails if condition is true
+
+  playerPos = 5;
+  let condition = () => { //hace algo si el player está cerca
+    if (playerPos < 3) {
+      return true;
+    }
+
+    return false;
+  };
+
+  let decorator = new DecoratorNode(condition, child);
+  assertEQ(returnStatement.SUCCESS, decorator.tick({}),
+    'Decorator evalueates condition succesfully if false');
+  playerPos = 1;
+  assertEQ(returnStatement.RUNNING, decorator.tick({}),
+    'Decorator evalueates condition succesfully if true');
+}
+
+function treeTests() {
+  console.log('\n DECORATOR TESTS');
+  let action = new ActionNode(() => {
+    return returnStatement.RUNNING;
+  });
+  let child = new CompositeNode(new Map(
+    [
+      ['action', action],
+    ])); //so it fails if condition is true
+
+  playerPos = 5;
+  let condition = () => { //hace algo si el player está cerca
+    if (playerPos < 3) {
+      return true;
+    }
+
+    return false;
+  };
+
+  let decorator = new DecoratorNode(condition, child);
+  assertEQ(returnStatement.SUCCESS, decorator.tick({}),
+    'Decorator evalueates condition succesfully if false');
+  playerPos = 1;
+  assertEQ(returnStatement.RUNNING, decorator.tick({}),
+    'Decorator evalueates condition succesfully if true');
+}
