89939e59044aba5e37538a69d9cdd02af42e71a5
flortincab <flortincab@gmail.com>
2020-03-27 18:57:43 -0300

    add Tomb to scene. Tomb has dug level

--- engine/stuffCollectorGame/platforms/Tomb.js
+++ engine/stuffCollectorGame/platforms/Tomb.js
@@ -2,5 +2,13 @@ class Tomb extends Sprite {
     constructor() {
         super();
         this.init(2,1.3,"space_color", 1,1);
+        this.level = 4;
+
+        let interactiveObj = new Interactive(2,1,1,function(){
+        	if(this.object.level>0) this.object.level-=gDeltaTime;
+        	console.log(this.object.level);});
+        interactiveObj.setObject(this);
+        gDeveloperTools.drawHitbox(interactiveObj);
+        this.addChild(interactiveObj);
     }
 }
