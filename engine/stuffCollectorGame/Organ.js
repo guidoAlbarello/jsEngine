a29941cf5bdcd0bcb21d5c850cd2025f46e0813a
flortincab <flortincab@gmail.com>
2020-03-29 21:50:36 -0300

    Tomb changes color

--- engine/stuffCollectorGame/Organ.js
+++ engine/stuffCollectorGame/Organ.js
@@ -1,7 +1,7 @@
 class Organ extends Sprite {
     constructor() {
         super();
-        this.init(1,1,"space_color", 1,1);
+        this.init(1,1,"green", 1,1);
         this.type = gOrgansType[Math.floor(Math.random() * gOrgansType.length)];
 
         let collider = new Collider("player");
--- engine/stuffCollectorGame/platforms/Tomb.js
+++ engine/stuffCollectorGame/platforms/Tomb.js
@@ -2,11 +2,15 @@ class Tomb extends Sprite {
     constructor() {
         super();
         this.init(2,1.3,"space_color", 1,1);
+
         this.level = 3;
+        this.colorLevel = ["brown", "violet", "blue","space_color"];
+
         this.empty = false;
 
         let interactiveObj = new Interactive(2,1,1,function(){
-        	if(this.object.level>0) this.object.level-=gDeltaTime;});
+        	if(this.object.level<0) this.remove();
+        	this.object.level-=gDeltaTime;});
         interactiveObj.setObject(this);
         gDeveloperTools.drawHitbox(interactiveObj);
         this.addChild(interactiveObj);
@@ -23,6 +27,7 @@ class Tomb extends Sprite {
             	this.addChild(organ);
             	this.empty = true;
             }
+            this.material.setTextureAtlas(this.colorLevel[Math.ceil(this.level)]);
         });
 
         return behaviour;
